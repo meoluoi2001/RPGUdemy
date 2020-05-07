dab8fb3e03727e0159928d0c95f2ce640557fc03
meoluoi2001 <huyhoangvietha@gmail.com>
2020-05-06 21:48:37 -0400

    Create Health.cs

---
+++ RPG/Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RPG.Combat
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] private float health = 100f;
+    }
+}
